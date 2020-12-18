3e084a9675b10c3ca83a4439c4f67d2c97105699
Danielle H <60787559+smallscripter@users.noreply.github.com>
2020-12-17 20:57:13 -0500

    renamed variables to become clearer (ex. "num" to "targetNumber")

--- binary-search.java
+++ binary-search.java
@@ -2,18 +2,18 @@
 // used for: sorted lists
 // returns: index of a number in an array
 
-public static int indexInArray(int num, ArrayList<Integer> array) {
+public static int indexInArray(int targetNumber, ArrayList<Integer> array) {
     int minIndex = 0;
     int maxIndex = array.size()-1;
 
     while (minIndex <= maxIndex) {
         int midIndex = (minIndex + maxIndex) / 2;
-        int numGuess = array.get(midIndex);
-        if (num == numGuess)
+        int guessNumber = array.get(midIndex);
+        if (targetNumber == guessNumber)
             return midIndex;
-        if (num < numGuess)
+        if (targetNumber < guessNumber)
             maxIndex = midIndex - 1;
-        if (num > numGuess)
+        if (targetNumber > guessNumber)
             minIndex = midIndex + 1;
     }
     return -1;
